e2ec0308a800d89179b6448d1bdd0147ef2bb928
amandawraymond <amandawraymond@gmail.com>
2014-11-14 17:17:02 -0500

    seeded data and migrated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Question.destroy_all
+Option.destroy_all
+
+Question.create(question_number: 1, name:"name", label:"What is your full name?", input_type:"text")
+question = Question.create(question_number: 2, name:"skill", label:"What is your strongest skill as a Developer?", input_type:"select")
+question.options.create(answer:"Front End", weight:"1")
+question.options.create(answer:"Back End", weight:"1")
+question.options.create(answer:"Full Stack", weight:"1")
+question = Question.create(question_number: 3, name:"companies", label:"What type of companies have you worked at?", input_type:"radio")
+question.options.create(answer:"Startups", weight:"1")
+question.options.create(answer:"Small and Mid-Size", weight:"1")
+question.options.create(answer:"Large Enterprises", weight:"1")
+
